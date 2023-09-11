83dd02a722d770c465466adadf8cb7e2421a8c62
Kucha82 <143359502+Kucha82@users.noreply.github.com>
2023-09-10 23:03:15 -0300

    new

---
+++ new.py
@@ -0,0 +1,5 @@
+from paquete1.cliente import Cliente
+
+moni = Cliente("moni", "velez", "jfkdf@gkfl.com", "djskdj   234")
+
+print(F"nombre: {moni.nombre}\napellido: {moni.apellido}\nemail: {moni.email}")
